4119d21bf700a840d4072241e22a764d3191ef81
Pappantelis <pappantelispap@gmail.com>
2023-09-06 13:23:16 +0300

    My first commit

---
+++ cup_c_me/src/App.js
@@ -0,0 +1,14 @@
+import menu from "./menu.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={menu} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
